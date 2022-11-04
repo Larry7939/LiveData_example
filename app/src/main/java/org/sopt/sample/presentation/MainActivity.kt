1609f633d9ef803b8f92c93f00f3e66fa50ece96
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-11-04 23:54:04 +0900

    add : Base 초기 세팅

---
+++ app/src/main/java/org/sopt/sample/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.sopt.sample.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.sample.R
+import org.sopt.sample.base.BindingActivity
+import org.sopt.sample.databinding.ActivityLivedataBinding
+
+class LiveDataActivity : BindingActivity<ActivityLivedataBinding>(R.layout.activity_livedata) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
