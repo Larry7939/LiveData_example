6e10bc998fb4fd06c9530f47f15d5a60b7f5f106
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-11-09 09:56:54 +0900

    add : 양방향 데이터 바인딩

--- app/src/main/java/org/sopt/sample/presentation/twowaylivedata/TwoWayLiveDataActivity.kt
+++ app/src/main/java/org/sopt/sample/presentation/twowaylivedata/TwoWayLiveDataActivity.kt
@@ -1,11 +1,13 @@
 package org.sopt.sample.presentation.twowaylivedata
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import org.sopt.sample.R
 import org.sopt.sample.base.BindingActivity
 import org.sopt.sample.databinding.ActivityTwowaylivedataBinding
+import org.sopt.sample.presentation.mediator.MediatorActivity
 
 class TwoWayLiveDataActivity :
     BindingActivity<ActivityTwowaylivedataBinding>(R.layout.activity_twowaylivedata) {
@@ -17,8 +19,12 @@ class TwoWayLiveDataActivity :
 
 
 //      양방향 binding
-        viewModel.inputText.observe(this) {
-            Log.d("값 변화", it)
+//        viewModel.inputText.observe(this) {
+//            Log.d("값 변화", it)
+//        }
+        binding.btnToway.setOnClickListener {
+            startActivity(Intent(this, MediatorActivity::class.java))
+            finish()
         }
     }
 }
