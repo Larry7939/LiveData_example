0ae91254d1121adddd69726a02e732bf21c0e951
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-11-09 09:57:08 +0900

    add : MediatorLiveData 예제

--- app/src/main/java/org/sopt/sample/presentation/mediator/MediatorActivity.kt
+++ app/src/main/java/org/sopt/sample/presentation/mediator/MediatorActivity.kt
@@ -1,12 +1,25 @@
 package org.sopt.sample.presentation.mediator
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import org.sopt.sample.R
 import org.sopt.sample.base.BindingActivity
 import org.sopt.sample.databinding.ActivityMediatorBinding
+import org.sopt.sample.util.showToast
 
 class MediatorActivity : BindingActivity<ActivityMediatorBinding>(R.layout.activity_mediator) {
+    private val viewModel: MediatorViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+
+        //1. xml의 로그인 버튼 클릭 시 MediatorViewModel의 isEnabledLoginButton() 호출
+        //2. MediatorActivity에서는 1번 과정을 통해 변경될 isInputValid만 observe
+        //3. isInputValid가 변경되면 토스트 메시지 출력
+        viewModel.isInputValid.observe(this){
+            if(it) showToast("로그인 성공!")
+            else showToast("로그인 실패!")
+        }
     }
 }
--- app/src/main/java/org/sopt/sample/presentation/mediator/MediatorViewModel.kt
+++ app/src/main/java/org/sopt/sample/presentation/mediator/MediatorViewModel.kt
@@ -1,7 +1,24 @@
 package org.sopt.sample.presentation.mediator
 
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import org.sopt.sample.util.addSourceList
 
 class MediatorViewModel:ViewModel() {
+    val id:MutableLiveData<String> = MutableLiveData<String>()
+    val pw:MutableLiveData<String> = MutableLiveData<String>()
+
+    val isInputValid:MediatorLiveData<Boolean> = MediatorLiveData<Boolean>()
+    fun isEnabledLoginButton(){
+        isInputValid.apply {
+            addSource(id){value = isValidSignInfo()}
+            addSource(pw){value = isValidSignInfo()}
+        }
+        //MediatorLiveDataExt에서 정의한 확장함수로 여러 개의 LiveData 인자를 받을 수 있다.
+        //isInputValid.addSourceList(id,pw){isValidSignInfo()}
+    }
+    fun isValidSignInfo() = !id.value.isNullOrBlank() && !pw.value.isNullOrBlank()
+
 
 }
