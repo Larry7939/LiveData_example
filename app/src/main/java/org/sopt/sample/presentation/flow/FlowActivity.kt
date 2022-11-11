7df316417e533e79b80128b7265588ad337a76aa
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-11-12 02:40:48 +0900

    Update FlowActivity.kt

--- app/src/main/java/org/sopt/sample/presentation/flow/FlowActivity.kt
+++ app/src/main/java/org/sopt/sample/presentation/flow/FlowActivity.kt
@@ -1,7 +1,10 @@
 package org.sopt.sample.presentation.flow
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -13,7 +16,15 @@ import org.sopt.sample.base.BindingActivity
 import org.sopt.sample.databinding.ActivityFlowBinding
 
 class FlowActivity : BindingActivity<ActivityFlowBinding>(R.layout.activity_flow) {
+    private val viewModel: FlowViewModel by viewModels()
+
     private val stringFlow : Flow<String> = flow<String> {
+        for (i in 0..1000){
+            emit("integer : $i")
+            delay(1000)
+        }
+    }.flowOn(Dispatchers.IO)
+    private val stringFlow2 : Flow<String> = flow<String> {
         for (i in 0..1000){
             emit("integer : $i")
             delay(1000)
@@ -21,19 +32,22 @@ class FlowActivity : BindingActivity<ActivityFlowBinding>(R.layout.activity_flow
     }.flowOn(Dispatchers.IO)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+
         lifecycleScope.launch{
-//            repeatOnLifecycle(Lifecycle.State.STARTED){
-//                launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                launch {
                     stringFlow.collect{
                         println(it)
                     }
-//                }
-//                launch {
-//                    stringFlow2.collect{
-//                        println(it)
-//                    }
-//                }
-//            }
+                }
+                launch {
+                    stringFlow2.collect{
+                        println(it)
+                    }
+                }
+            }
         }
     }
 }
