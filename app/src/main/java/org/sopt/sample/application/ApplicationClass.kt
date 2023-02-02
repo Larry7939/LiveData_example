f6640ac7e4cec67097ac4427d5ca54c379fa8703
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-02-03 00:42:52 +0900

    Flow 예제 작성
    - flowWithLifecycle
    - repeatOnLifecycle
    - lifecycleScope.launch
    - lifecycleScope.launchWhenStarted

--- app/src/main/java/org/sopt/sample/application/ApplicationClass.kt
+++ app/src/main/java/org/sopt/sample/application/ApplicationClass.kt
@@ -1,9 +1,16 @@
 package org.sopt.sample.application
 
 import android.app.Application
+import org.sopt.sample.BuildConfig
+import timber.log.Timber
 
 class ApplicationClass : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
 
-
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
--- app/src/main/java/org/sopt/sample/presentation/flow/FlowActivity.kt
+++ app/src/main/java/org/sopt/sample/presentation/flow/FlowActivity.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -14,39 +15,62 @@ import kotlinx.coroutines.launch
 import org.sopt.sample.R
 import org.sopt.sample.base.BindingActivity
 import org.sopt.sample.databinding.ActivityFlowBinding
+import timber.log.Timber
 
 class FlowActivity : BindingActivity<ActivityFlowBinding>(R.layout.activity_flow) {
     private val viewModel: FlowViewModel by viewModels()
 
-    private val stringFlow : Flow<String> = flow<String> {
-        for (i in 0..1000){
-            emit("integer : $i")
+    /**Local or Remote DataSource**/
+    private val stringFlow: Flow<String> = flow<String> {
+        for (i in 0..1000) {
+            emit("$i")
             delay(1000)
         }
     }.flowOn(Dispatchers.IO)
-    private val stringFlow2 : Flow<String> = flow<String> {
-        for (i in 0..1000){
-            emit("integer : $i")
+    private val stringFlow2: Flow<String> = flow<String> {
+        for (i in 0..1000) {
+            emit("$i")
+            Timber.d("${currentCoroutineContext()}")
             delay(1000)
         }
     }.flowOn(Dispatchers.IO)
+    private val stringFlow3: Flow<String> = flow<String> {
+        for (i in 0..1000) {
+            emit("$i")
+            delay(1000)
+        }
+    }.flowOn(Dispatchers.IO)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = viewModel
         binding.lifecycleOwner = this
 
-        lifecycleScope.launch{
-            repeatOnLifecycle(Lifecycle.State.STARTED){
+//        lifecycleScope.launch {
+//            stringFlow.flowWithLifecycle(lifecycle,Lifecycle.State.STARTED).collect{
+//            }
+//        }
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
                 launch {
-                    stringFlow.collect{
-                        println(it)
+                    stringFlow.collect {
+                        binding.tvFlowRepeatValue.text = it
+                        Timber.d("repeatOnLifecycle : $it")
                     }
                 }
-                launch {
-                    stringFlow2.collect{
-                        println(it)
-                    }
-                }
+            }
+        }
+        lifecycleScope.launch {
+            stringFlow2.collect {
+                binding.tvFlowLaunchValue.text = it
+                Timber.d("${this.coroutineContext}")
+                Timber.d("launch : $it")
+            }
+        }
+        lifecycleScope.launchWhenStarted {
+            stringFlow3.collect {
+                binding.tvFlowLaunchWhenStartedValue.text = it
+                Timber.d("launchWhenStarted : $it")
             }
         }
     }
